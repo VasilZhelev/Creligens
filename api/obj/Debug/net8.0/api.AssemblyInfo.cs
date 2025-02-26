ebf80a6270bd8bd955870214e7dc408ccef3c773
BorisVelinov <151785856+BorisVelinov@users.noreply.github.com>
2025-02-26 22:11:17 +0200

    .

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3353cc18b8ed9dfc0ca282cb371c623b3f5393e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d06b1d1c45e3008ec31c190449f060f10600dfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
