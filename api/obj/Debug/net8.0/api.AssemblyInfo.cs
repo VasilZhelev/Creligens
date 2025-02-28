19dcd1c657c8e69e5091797f9a6c1524086e028d
BorisVelinov <151785856+BorisVelinov@users.noreply.github.com>
2025-02-28 12:31:15 +0200

    delite

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07c0d03753a67d1e9e98f89a4c3f403bbcf148e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2762a0fb4ce02de45929c60a25a796d4f6e9070f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
