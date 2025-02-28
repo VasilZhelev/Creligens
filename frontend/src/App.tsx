8cc91b78d3cb1cf8a728b5f326053dd73721af1e
i-georgiev28 <IvanGeorgiev28@itpg-varna.bg>
2025-02-28 14:32:28 +0200

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Index from '@/routes/Index';
+
+export default function App() {
+    return <>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Index />} />
+            </Routes>
+        </BrowserRouter>
+    </>;
+}
